3fffa58748b9093472e5adaa56969b39bbde13a2
Mark Hesser <mark@hessercan.com>
2019-03-20 14:45:06 -0400

    Functions and LeapYear Example

---
+++ functions.py
@@ -0,0 +1,20 @@
+# Sample Functions in Python
+
+
+def main(j, k):
+    for i in range(j,k):
+        # Odd A
+        if i % 2 != 0:
+            functionA()
+        # Even B
+        else:
+            functionB()
+
+def functionA():
+    print("Hi, this is Function Aaa")
+def functionB():
+    print("Hi, this is Function Bee")
+
+main(1,21)
+
+quit()
